735af6c3e077f85ccbbcdf56f1ffe5666458d34f
MossDream <124897852+MossDream@users.noreply.github.com>
2023-05-02 13:47:58 +0800

    Update project.c

--- Program1/project.c
+++ Program1/project.c
@@ -78,4 +78,82 @@ int main()
     return 0;
 }
 // 功能函数实现
-// 读取一个单词,用二进制读入，同时要把单词转换成小写
+// 读取一个单词,同时要把单词转换成小写
+void GetWord(FILE *file)
+{
+    int i = 0;
+    char ch;
+    while ((ch = fgetc(file)) != EOF)
+    {
+        if (isalpha(ch))
+        {
+            word[i++] = tolower(ch);
+        }
+        else
+        {
+            if (i > 0)
+            {
+                word[i] = '\0';
+                return;
+            }
+        }
+    }
+    if (i > 0)
+    {
+        word[i] = '\0';
+    }
+}
+// 创建停用词树
+void CreateStopWordsTree()
+{
+    char word[20] = {0};
+    StopWordsTree *p;
+    while (fscanf(StopWordsFile, "%s", word) != EOF)
+    {
+        p = Root;
+        while (p != NULL)
+        {
+            if (strcmp(word, p->word) < 0)
+            {
+                if (p->left == NULL)
+                {
+                    p->left = (StopWordsTree *)malloc(sizeof(StopWordsTree));
+                    strcpy(p->left->word, word);
+                    p->left->left = NULL;
+                    p->left->right = NULL;
+                    break;
+                }
+                else
+                {
+                    p = p->left;
+                }
+            }
+            else if (strcmp(word, p->word) > 0)
+            {
+                if (p->right == NULL)
+                {
+                    p->right = (StopWordsTree *)malloc(sizeof(StopWordsTree));
+                    strcpy(p->right->word, word);
+                    p->right->left = NULL;
+                    p->right->right = NULL;
+                    break;
+                }
+                else
+                {
+                    p = p->right;
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+        if (p == NULL)
+        {
+            Root = (StopWordsTree *)malloc(sizeof(StopWordsTree));
+            strcpy(Root->word, word);
+            Root->left = NULL;
+            Root->right = NULL;
+        }
+    }
+}
